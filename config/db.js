bc9518fe0f32328077592cc9c107febea4d93d03
Nabigha Mogharbel <nabighamogharbel@gmail.com>
2023-01-23 16:00:02 +0200

    basic set up in index.js and connect to database

---
+++ config/db.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const connectDB = async() => {
+try {
+    mongoose.set('strictQuery', false);
+    const conn = await mongoose.connect(process.env.MONGO, {
+        useUnifiedTopology: true,
+        dbName: process.env.DB_NAME
+    })
+    console.log(`Connected to: ${conn.connection.host}`);
+} catch (error) {
+    console.log(`Error: ${error.message}`)
+    process.exit();
+}
+}
+
+export default connectDB;
